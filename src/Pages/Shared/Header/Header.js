851628d4473b7b2d31967c57cb841cd0f7463968
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-26 22:41:52 +0600

    Installed react-icons

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 const Header = () => {
   return (
     <div>
       <h2>This is Header</h2>
+      <Button variant="outline-success">Login via Google</Button>
+      <Button variant="outline-success">Login via GitHub</Button>
     </div>
   );
 };
