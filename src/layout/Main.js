6ba9d7271fbaddfebb1809885a084fc8264da43f
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-26 15:10:23 +0600

    Basic Layout done

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import Header from "../Pages/Shared/Header/Header";
+import LeftSideNav from "../Pages/Shared/LeftSideNav/LeftSideNav";
 
 const Main = () => {
   return (
-    <Container>
-      <Row>
-        <Col lg="4">
-          <h2>Side Nav</h2>
-        </Col>
-        <Col lg="8">
-          <Outlet></Outlet>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <Header></Header>
+      <Container>
+        <Row>
+          <Col lg="4">
+            <LeftSideNav></LeftSideNav>
+          </Col>
+          <Col lg="8">
+            <Outlet></Outlet>
+          </Col>
+        </Row>
+      </Container>
+      <Footer></Footer>
+    </div>
   );
 };
 
