3d3c96941d267053fe418964eac2276ab66a2f08
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-26 14:30:25 +0600

    installed react-bootstrap bootstrap

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCApyxrBT1YPCkBcuPEyTt2H40Re1Ha2NA",
+  authDomain: "online-mentor-19470.firebaseapp.com",
+  projectId: "online-mentor-19470",
+  storageBucket: "online-mentor-19470.appspot.com",
+  messagingSenderId: "270844921908",
+  appId: "1:270844921908:web:cedbe317e79448b84aa261",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
