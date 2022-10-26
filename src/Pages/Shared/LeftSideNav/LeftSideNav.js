56deaa3b8b9eb13344621362a10b753e6bbb45f0
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-26 21:38:14 +0600

    courses length showing completed

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const LeftSideNav = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/courses")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
+
   return (
     <div>
-      <h2>Left Side Nav</h2>
+      <h2>All Courses : {courses.length}</h2>
     </div>
   );
 };
