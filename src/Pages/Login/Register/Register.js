666123cef666b5d651aef880d6c149746364c4e2
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-28 13:25:02 +0600

    Login and registration page setup

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,55 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Register = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <h2>This is Register page</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Full Name </Form.Label>
+          <Form.Control name="name" type="text" placeholder="Enter Name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPhotoUrl">
+          <Form.Label>PhotoURL </Form.Label>
+          <Form.Control
+            name="photoURL"
+            type="url"
+            placeholder="Enter PhotoUrl"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            name="email"
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            name="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+        <Form.Text className="text-danger">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form>
     </div>
   );
 };
